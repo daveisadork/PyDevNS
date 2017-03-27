8d47f1aec6347bd6d53f464b0f518ebab1def299
Dave Hayes <dwhayes@gmail.com>
2017-03-27 09:29:49 -0500

    Clean up help output a bit

--- devns/cli.py
+++ devns/cli.py
@@ -17,27 +17,27 @@ def parse_args(args=None, config=config):
         dest="log_level", help="quiet mode"
     )
 
-    listen = parser.add_argument_group()
-    listen.add_argument("--host", type=str, help="address to listen on")
-    listen.add_argument("--port", type=int, help="port to listen on")
-
-    parser.add_argument(
+    general = parser.add_argument_group("General")
+    general.add_argument(
         "--address", type=str, help="IP address to respond with"
     )
 
-    resolver_group = parser.add_mutually_exclusive_group()
+    listen = parser.add_argument_group("Network")
+    listen.add_argument("--host", type=str, help="address to listen on")
+    listen.add_argument("--port", type=int, help="port to listen on")
+
+    resolver_group = parser.add_argument_group("Resolver")
     resolver_group.add_argument(
-        "--no-resolver", "-nr", action="store_false", dest="resolver",
-        help="disable creating resolver files"
-    )
-    resolver_config = resolver_group.add_argument_group()
-    resolver_config.add_argument(
         "--domains", type=str, nargs="*", metavar="DOMAIN",
         help="domains to create resolver files for"
     )
-    resolver_config.add_argument(
+    resolver_group.add_argument(
         "--resolver-dir", type=str, metavar="DIRECTORY", dest="resolver_dir",
         help="where to put resolver files"
+    )
+    resolver_group.add_argument(
+        "--no-resolver", "-nr", action="store_false", dest="resolver",
+        help="disable creating resolver files"
     )
 
     parser.set_defaults(**config.DEFAULTS)
