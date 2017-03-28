7a763a5bb84f7b337eeb13c604d7a988d1c875e9
Dave Hayes <dave@ave81.com>
2017-03-28 06:06:01 -0500

    Bump version: 0.3.5 → 0.3.6

--- devns/__init__.py
+++ devns/__init__.py
@@ -38,7 +38,7 @@ config = Config()  # noqa
 
 
 __title__ = "devns"
-__version__ = "0.3.5"
+__version__ = "0.3.6"
 __author__ = "Dave Hayes"
 __license__ = "MIT"
 __copyright__ = "Copyright (c) 2017 Dave Hayes"
