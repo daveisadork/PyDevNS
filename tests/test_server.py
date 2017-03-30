f9f5a46ec8d3eb15a4617663ed3e47f12223d7f9
Dave Hayes <dave@ave81.com>
2017-03-30 11:46:43 -0500

    Remove erroneous debugging code

--- tests/test_server.py
+++ tests/test_server.py
@@ -76,11 +76,7 @@ def test_server_bind_error_bad_port(config, server, host, port):
     config.host = host
     config.port = port
     with server.bind() as connection:
-        try:
-            assert isinstance(connection, Exception)
-        except:
-            print(connection.getsockname())
-            raise
+        assert isinstance(connection, Exception)
 
 
 @pytest.mark.parametrize("domains", [("dev", ), ("dev", "local.co")])
