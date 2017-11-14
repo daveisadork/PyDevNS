4a434a62f10d9104af1d0263882cedf5b12bddae
Dave Hayes <dave@ave81.com>
2017-11-14 06:14:04 -0600

    Bump version: 0.4.6 → 0.4.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 # confused? check setup.cfg
 setup(
     name="devns",
-    version="0.4.6",
+    version="0.4.7",
     author="Dave Hayes",
     author_email="dwhayes@gmail.com",
     description="Pure Python DNS server for developers",
